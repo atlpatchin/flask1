e5f2a204a015d421596c0ba17bd2e4a0268a13b1
atlpatchin <atlpat@163.com>
2019-01-09 20:33:30 +0800

    user

--- flask-test/app/auth/forms.py
+++ flask-test/app/auth/forms.py
@@ -1,9 +1,9 @@
-from flask.ext.wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Length,Email,Regexp,EqualTo
 from wtforms import ValidationError
 from ..models import User
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[Required(),Length(1,64),Email()])
     username = StringField('Username',validators=[Required(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0,9.]*$',0,'Usernames must have only letters,numbers,dots or underscores')])
     password = PasswordField('Password', validators=[
@@ -17,8 +17,34 @@ class RegistrationForm(Form):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email',validators=[Required(),Length(1,64),Email()])
     password = PasswordField('Password',validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password',validators=[Required()])
+    password = PasswordField('New password',validators=[Required(),EqualTo('password2',message = 'Passwords must match')])
+    password2 = PasswordField('Confirm new password',validators = [Required()])
+    submit = SubmitField('Update Password')
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email',validators=[Required(),Length(1,64),Email()])
+    submit=SubmitField('Reset Password')
+
+class PasswordResetForm(FlaskForm):
+    email = StringField('Email',validators=[Required(),Length(1,64),Email()])
+    password = PasswordField('New Password',validators=[Required(),EqualTo('password2',message='Passwords must match')])
+    password2=PasswordField('Confirm password',validators=[Required()])
+    submit=SubmitField('Reset Password')
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first() is None:
+            raise ValidationError('Unknown email address.')
+
+class ChangeEmailForm(FlaskForm):
+    eamil = StringField('New Email',validators=[Required(),Length(1,64),Email()])
+    password = PasswordField('Password',validators=[Required()])
+    submit=SubmitField('Update Email Address')
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
--- flask-test/app/models.py
+++ flask-test/app/models.py
@@ -1,21 +1,55 @@
 from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask.ext.login import UserMixin
+from flask.ext.login import UserMixin,AnonymousUserMixin
 
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT =0x02
+    WRITE_ARTICLES = 0x08
+    ADMINISTER =0x80
+
 class Role(db.Model):
     __tablename__='roles'
     id = db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
+    default = db.Column(db.String(64),unique=True,index=True)
+    permissions = db.Column(db.Integer)
+
     def __repr__(self):
         return '<Role %r>' %self.name
 
     users = db.relationship('User',backref='role',lazy = 'dynamic')
+    @staticmethod
+    def insert_roles():
+        roles = {
+                'User':(Permission.FOLLOW |
+                        Permission.COMMENT |
+                        Permisssion.WRITE_ARTICLES,True
+                       ),
+                'Moderator':(
+                        Permission.FOLLOW |
+                        Permission.COMMENT |
+                        Permission.WRITE_ARTICLES |
+                        Permission.MODERATE_COMMENTS, False
 
+                    ),
+                'Administrator': (0xff, False)
+
+                }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permission = roles[r][0]
+            role.default = roles[r][0]
+            db.session.add(role)
+        db.session.commit()
 class User(UserMixin,db.Model):
     __tablename__='users'
     id = db.Column(db.Integer,primary_key=True)
@@ -49,3 +83,56 @@ class User(UserMixin,db.Model):
         self.confirmed = True
         db.session.add(self)
         return True
+    def __init__(self,**kwargs):
+        super(User,self).__init__(**kwargs)
+        if self.role is None:
+            if self.email==current_app.config['FLASKY_ADMIN']:
+                self.role==Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id})
+    def reset_password(self, token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+                 data = s.loads(token)
+        except:
+                 return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
+        db.session.add(self)
+        return True
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'change_email': self.id, 'new_email': new_email})
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('change_email')!=self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True
+    def can(self,permissions):
+         return self.role is not None and (self.role.permissions & permissions)==permissions
+    def is_administrator(self):
+         return self.can(Permission.ADMINISTER)
+class AnonymousUser(AnonymousUserMixin):
+    def can(self,permissions):
+        return False
+    def is_administrator(self):
+        return False
+login_manager.anonymous_user = AnonymousUser
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
